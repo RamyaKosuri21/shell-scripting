69b78136eaa52e757ec102678996c553631c443e
Ramyak03 <ramya.kosuri20@gmail.com>
2021-05-05 09:52:57 -0500

    Added some comments at the end of the file for better understanding

--- 04-read.sh
+++ 04-read.sh
@@ -4,3 +4,5 @@ read -p 'Enter your Name: ' name
 read -p 'Enter the Course: ' course
 
 echo -e "\n Name = $name , Course = $course"
+
+# this is not a great way of doing for automation, as we rae interrupting in between.We need to give input, but it should not interrupt at the same time
