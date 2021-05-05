a2e7da8baf04394d3e5691cb96b8323c091172c8
Ramyak03 <ramya.kosuri20@gmail.com>
2021-05-05 14:22:31 -0500

    removed code that used to check if the user is root user or not and kept in common place called common.sh

--- project/components/frontend.sh
+++ project/components/frontend.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 
-USER_ID=$(id -u)
-if [ "${USER_ID}" -ne 0 ] ; then
-  echo -e "\e[1;31m""you should be a root user to perform this command\e[0m"
-  exit
- fi
-
 yum install nginx -y
 
 # systemctl enable nginx
