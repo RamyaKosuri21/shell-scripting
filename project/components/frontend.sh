7d05ef7562f1beb76a483570e85a167a73db0173
Ramyak03 <ramya.kosuri20@gmail.com>
2021-05-05 15:02:30 -0500

    copied all the steps of the frontend and trying to run at glance.

--- project/components/frontend.sh
+++ project/components/frontend.sh
@@ -2,5 +2,18 @@
 
 yum install nginx -y
 
-# systemctl enable nginx
-# systemctl start nginx
+systemctl enable nginx
+systemctl start nginx
+
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+
+ cd /usr/share/nginx/html
+ rm -rf *
+ unzip /tmp/frontend.zip
+ mv frontend-main/* .
+ mv static/* .
+ rm -rf frontend-master README.md
+ mv localhost.conf /etc/nginx/default.d/roboshop.conf
+
+
+ systemctl restart nginx
