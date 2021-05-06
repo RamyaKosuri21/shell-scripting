95be9556af0ad99032103862bbcaafc653e06fd4
Ramyak03 <ramya.kosuri20@gmail.com>
2021-05-05 22:03:15 -0500

    Added code to tag the names while creating instance

--- instance-create/create.sh
+++ instance-create/create.sh
@@ -2,6 +2,13 @@
 
 LID=lt-008b4a05314d92570
 LVER=1
-aws ec2 run-instances --launch-template LaunchTemplateId=${LID},Version=${LVER} | jq
+COMPONENT=$1
 
+if [ -z "${COMPONENT}" ]; then
+  echo "Component Name Input is needed"
+  exit 1
+fi
+
+aws ec2 run-instances --launch-template LaunchTemplateId=${LID},Version=${LVER}  --tag-specifications "ResourceType=instance,
+Tags=[{Key=Name,Value=${COMPONENT}}]" | jq
 
